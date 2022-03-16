8b79be5692c23841ba7f8539def9f5e0a9ab425e
morpheusos <eulenberg.tim@gmail.com>
2022-03-16 22:43:39 +0100

    first attempt on pydeck

--- website.py
+++ website.py
@@ -8,6 +8,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 import math
+import pydeck as pdk
 
 #%% website
 
@@ -45,13 +46,68 @@ st.text('this is the sample frame you see on the widget above:')
 demo_frame
 
 #%%% the search "engine"
-our_frame = pd.read_csv('regressions.csv')
+our_frame = pd.read_csv('C:/Users/x-tim/Downloads/website_frame.csv')
 street = st.text_input('Enter a streetname to see its data')
-if (our_frame['name'].str.contains(street).sum()) > 0: 
-    result = our_frame[our_frame['name'] == street]
+if (our_frame['street_name'].str.contains(street).sum()) > 0: 
+    result = our_frame[our_frame['street_name'] == street]
     st.write(result)
 elif street == '':
     #nothing entered no exception needed.
     nonsense = 0
 else:
-    st.error('there is no street with this name in the dataset, check for spelling or try a different street')
+    st.error('there is no street with this street_name in the dataset, check for spelling or try a different street')
+    
+#%%% second attempt for a map
+# layer = pdk.Layer(
+#     'HexagonLayer',  # `type` positional argument is here
+#     our_frame,
+#     get_position=['osm_start_node_id'],
+#     auto_highlight=True,
+#     elevation_scale=50,
+#     pickable=True,
+#     elevation_range=[0, 3000],
+#     extruded=True,
+#     coverage=1)
+
+# # Set the viewport location
+# view_state = pdk.ViewState(
+#     longitude=-1.415,
+#     latitude=52.2323,
+#     zoom=6,
+#     min_zoom=5,
+#     max_zoom=15,
+#     pitch=40.5,
+#     bearing=-27.36)
+
+# # Combined all of it and render a viewport
+# r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+# st.write(r)
+
+#####
+df = our_frame
+#  pd.DataFrame(
+#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#     columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/streets-v11',
+     initial_view_state=pdk.ViewState(
+         latitude=37.76,
+         longitude=-122.4,
+         zoom=11,
+         pitch=50,
+     ),
+     layers=[
+         pdk.Layer(
+            'HexagonLayer',
+            data=df,
+            get_position='osm_start_node_id',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+         ),
+     ],
+ )
+)
