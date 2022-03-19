731ea65331c39984e702c4fb2967f89e78d15627
morpheusos <92370102+morpheusos@users.noreply.github.com>
2022-03-19 19:21:01 +0100

    Update website.py

--- website.py
+++ website.py
@@ -27,23 +27,9 @@ st.header('Traffic and weather analysis')
 st.text(
 """
 We used a uber dataset with information on movement speeds and scraped weather data to build a regression model that allowed us to analize to what degree a street is affected by the weather conditions.
-The following dataframe represents our data, but so far i dont know the latitude and longitude of our nodes. Use the slider to see to which degree which coordinates are affected.
+To look at specific data enter a street name below. The column named rsquared is the indicator on how weather resistant the traffic on the street is with a range from 0 to 1.
 """
 )
-
-#%%% the map
-demo_frame = pd.DataFrame({
-    'lat': [3.47659, 3.48679, 3.59603, 3.59867, 3.00222, 3.85622, 3.55720],
-    'lon': [23.87260, 23.90500, 23.90532, 23.77616, 23.32388, 3.84777, 23.12211],
-    'rsquared': [0.13, 0.15, 0.22, 0.0, 0.11, 0.31, 0.225] 
-    })
-demo_frame['rsquared_inverted_floor'] = demo_frame['rsquared'].apply(lambda x: math.floor((1-x)*10))
-x = st.slider('affectedness by weather', max_value = 10)
-st.map(demo_frame[demo_frame['rsquared_inverted_floor'] == x])
-
-#%%% the frame
-st.text('this is the sample frame you see on the widget above:')
-demo_frame
 
 #%%% the search "engine"
 our_frame = pd.read_csv('regressions.csv')
@@ -57,16 +43,33 @@ elif street == '':
 else:
     st.error('there is no street with this name in the dataset, check for spelling or try a different street')
     
+
+#%%% the map
+st.text("Since our equipment/chrome/the python API for open street map did not support exctracting that much data the following maps are examples for how our data could be represented (We didn't manage to exctract the coordinates for each street.)
+st.header("Map style 1 (Slider)")
+demo_frame = pd.DataFrame({
+    'lat': [3.47659, 3.48679, 3.59603, 3.59867, 3.00222, 3.85622, 3.55720],
+    'lon': [23.87260, 23.90500, 23.90532, 23.77616, 23.32388, 3.84777, 23.12211],
+    'rsquared': [0.13, 0.15, 0.22, 0.0, 0.11, 0.31, 0.225] 
+    })
+demo_frame['rsquared_inverted_floor'] = demo_frame['rsquared'].apply(lambda x: math.floor((1-x)*10))
+x = st.slider('affectedness by weather', max_value = 10)
+st.map(demo_frame[demo_frame['rsquared_inverted_floor'] == x])
+
+#%%% the frame
+st.text('this is the sample frame you see on the map above:')
+demo_frame
+
+
 #%%% second attempt for a map
-#CATTLE_DATA = "https://raw.githubusercontent.com/ajduberstein/geo_datasets/master/nm_cattle.csv"
+st.header("Map style 2 (Heatmap)")
+st.text("this is another (bigger) example dataframe from the internet")
 POULTRY_DATA = "https://raw.githubusercontent.com/ajduberstein/geo_datasets/master/nm_chickens.csv"
 
-
 HEADER = ["lng", "lat", "weight"]
-#cattle_df = pd.read_csv(CATTLE_DATA, header=None).sample(frac=0.5)
+
 poultry_df = pd.read_csv(POULTRY_DATA, header=None).sample(frac=0.5)
 
-#cattle_df.columns = HEADER
 poultry_df.columns = HEADER
 
 COLOR_BREWER_BLUE_SCALE = [
@@ -81,20 +84,6 @@ COLOR_BREWER_BLUE_SCALE = [
 
 view = pdk.data_utils.compute_view(poultry_df[["lng", "lat"]])
 view.zoom = 6
-
-"""
-cattle = pdk.Layer(
-    "HeatmapLayer",
-    data=cattle_df,
-    opacity=0.9,
-    get_position=["lng", "lat"],
-    aggregation=pdk.types.String("MEAN"),
-    color_range=COLOR_BREWER_BLUE_SCALE,
-    threshold=1,
-    get_weight="weight",
-    pickable=True,
-)
-"""
 
 poultry = pdk.Layer(
     "HeatmapLayer",
@@ -117,3 +106,7 @@ r = pdk.Deck(
 )
 
 st.write(r)
+
+st.header("Our whole dataframe:")
+our_frame        
+ 
